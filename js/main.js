906bad01472ed6a1470eb700526bcc3dc6480e33
Ravil Shamilov <75186385+Ravil377@users.noreply.github.com>
2021-12-14 16:37:20 +0500

    fix slider

--- js/main.js
+++ js/main.js
@@ -175,50 +175,40 @@ document.addEventListener(
         const swiperPack = document.querySelector(".swiper-dots");
         const swiperPackDotContainer = document.querySelector(".swiper-pagination-dots");
         const swiperSlide = swiperPack.querySelectorAll(".swiper-slide");
-        
+        const containerSlider = document.querySelector('.carousel');
 
 
-        var Visible = function (target) {
+        var Visible = function (target, contai) {
             // Все позиции элемента
             var targetPosition = {
-                    top: window.pageYOffset + target.getBoundingClientRect().top,
                     left: window.pageXOffset + target.getBoundingClientRect().left,
-                    right: window.pageXOffset + target.getBoundingClientRect().right,
-                    bottom: window.pageYOffset + target.getBoundingClientRect().bottom,
+                    right: window.pageXOffset + target.getBoundingClientRect().right
                 },
                 // Получаем позиции окна
+                container = {
+                    left: window.pageXOffset + contai.getBoundingClientRect().left,
+                    right: window.pageXOffset + contai.getBoundingClientRect().right
+                }
                 windowPosition = {
-                    top: window.pageYOffset,
                     left: window.pageXOffset,
-                    right: window.pageXOffset + document.documentElement.clientWidth,
-                    bottom: window.pageYOffset + document.documentElement.clientHeight,
+                    right: window.pageXOffset + document.documentElement.clientWidth
                 };
-
-            if (
-                targetPosition.bottom > windowPosition.top && // Если позиция нижней части элемента больше позиции верхней чайти окна, то элемент виден сверху
-                targetPosition.top < windowPosition.bottom && // Если позиция верхней части элемента меньше позиции нижней чайти окна, то элемент виден снизу
-                targetPosition.left > windowPosition.left && // Если позиция правой стороны элемента больше позиции левой части окна, то элемент виден слева
-                targetPosition.right < windowPosition.right
-            ) {
-                // Если позиция левой стороны элемента меньше позиции правой чайти окна, то элемент виден справа
-                // Если элемент полностью видно, то запускаем следующий код
+            if (targetPosition.right >= container.left && targetPosition.left <= container.right) {
                 return true;
             } else {
-                // Если элемент не видно, то запускаем этот код
                 return false;
             }
         };
         const resetDot = () => {
-            
             swiperSlide.forEach((slide) => {
                 swiperPackDotContainer.querySelector(`[data-dotid="${slide.dataset.slideid}"]`).classList.remove("swiper-pagination-bullet-active");
             });
         };
 
         const handler = () => {
-            // resetDot();
+            resetDot();
             swiperSlide.forEach((slide) => {
-                if (Visible(slide)) {
+                if (Visible(slide, containerSlider)) {
                     document.querySelector(`[data-dotid="${slide.dataset.slideid}"]`).classList.add("swiper-pagination-bullet-active");
                 }
             });
